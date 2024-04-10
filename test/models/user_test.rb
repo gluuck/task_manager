a747ba491572006524eb6e3e3655c4d0800ac03b
Vladimir Ostapenko <ostapenkov74@gmail.com>
2024-04-10 16:45:25 +0500

    fix

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,19 +1,7 @@
 require "test_helper"
 
-class UserTest < ActiveSupport::TestCase
-  def setup
-    @user = User.create(email: "test@example.com", password: '123456')
-    p @user
+class UserTest < ActiveSupport::TestCase  
+  test "create" do
+    assert @user.persisted?
   end
-  
-  # test "create" do
-  #   assert @user.persisted?
-  # end
-
-  def teardown
-    
-    debugger
-    @user.destroy
-  end
-  
 end
