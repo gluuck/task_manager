14c7c1adb34c7e5b3cff86c178fe0466cc8b7994
Vladimir Ostapenko <ostapenkov74@gmail.com>
2024-04-19 08:30:54 +0500

    fix validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ApplicationRecord
   has_many :my_tasks, class_name: 'Task', foreign_key: :author_id
   has_many :assigned_tasks, class_name: 'Task', foreign_key: :assignee_id
 
-  validates :first_name, presence: true, length: { minimum: 2 }
-  validates :last_name, presence: true, length: { minimum: 2 }
+  validates :first_name, :last_name, presence: true, length: { minimum: 2 }
   validates :type, presence: true
   validates :password, presence: true, length: { minimum: 6 }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, uniqueness: true, presence: true
