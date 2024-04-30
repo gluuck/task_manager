c717a8bbd80537299b7f0d4d0f1c5bc7d167867b
Vladimir Ostapenko <ostapenkov74@gmail.com>
2024-04-30 13:41:50 +0500

    Add index action & serializer

---
+++ app/controllers/api/v1/tasks_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::TasksController < class Api::V1::ApplicationController
+
+  def index
+    tasks = Task.all
+                .ransack(ransack_params)
+                .result
+                .page(page)
+                .per(per_page)
+
+    respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
+  end
+end
