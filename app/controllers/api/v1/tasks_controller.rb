e12ba07a5630d20a85928e287251c20fa7e38b13
Vladimir Ostapenko <ostapenkov74@gmail.com>
2024-04-30 13:43:30 +0500

    Add show action

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -9,4 +9,9 @@ class Api::V1::TasksController < class Api::V1::ApplicationController
 
     respond_with(tasks, each_serializer: TaskSerializer, root: 'items', meta: build_meta(tasks))
   end
+
+  def show
+    task = Task.find(params[:id])
+    respond_with(task, serializer: TaskSerializer)
+  end
 end
